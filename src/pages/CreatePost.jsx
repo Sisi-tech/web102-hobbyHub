36dcc58ed0855e2e6e06fea17cb6c3430b6c8ce3
Sisi Wang <sisiwang242@gmail.com>
2024-04-14 22:46:13 -0400

    add supabase

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
+import { supabase } from "../client";
 
 const CreatePost = () => {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
-    const [url, setUrl] = useState("");
+    const [post, setPost] = useState({title: "", content: "", imgUrl: ""});
+
+    const createPost = async (e) => {
+        e.preventDefault();
+        await supabase
+            .from("hubbyHub")
+            .insert({title: post.title, content: post.content, imgUrl: post.imgUrl});
+
+        window.location = "/";
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setPost((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
 
     return (
         <div className="h-screen flex flex-col gap-6 justify-center items-center">
@@ -12,25 +28,30 @@ const CreatePost = () => {
                 <input 
                     type="text" 
                     placeholder="Title" 
-                    value={title} 
+                    value={post.title} 
                     className='p-2 border-2 rounded-xl'
+                    onChange={handleChange}
                 />
                 <textarea
                     type="text" 
                     rows="8" 
                     cols="60" 
                     placeholder="Content(Optional)" 
-                    value={content} 
+                    value={post.content} 
                     className="p-2 border-2 rounded-xl"
+                    onChange={handleChange}
                 />
                 <input 
                     type="text" 
                     placeholder="Image URL(Optional)" 
                     className="p-2 border-2 rounded-xl"
+                    value={post.imgUrl}
+                    onChange={handleChange}
                 />
                 <button 
                     type="button"
                     className="bg-emerald-400 w-[200px] p-2 border-2 rounded-xl"
+                    onClick={createPost}
                 >
                     Create Post
                 </button>
